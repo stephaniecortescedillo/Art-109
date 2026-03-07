d5c55c3dcd42c520242dc310d90d31de615d3bd7
stephaniecortescedillo <stephanie.cortescedillo@sjsu.edu>
2026-03-06 16:19:06 -0800

    lightbox

---
+++ tutorials/2-css/07-Portfolio/JS/hamburger.js
@@ -0,0 +1,8 @@
+
+
+
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.nav-menu');
+hamburger.addEventListener('click', ()=> {
+   menu.classList.toggle('active');
+})
