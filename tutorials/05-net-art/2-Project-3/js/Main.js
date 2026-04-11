c1c55e5c44617efc15ce43e9bfb5026346d4014d
stephaniecortescedillo <stephanie.cortescedillo@sjsu.edu>
2026-04-10 21:50:58 -0700

    p3

--- tutorials/05-net-art/2-Project-3/js/Main.js
+++ tutorials/05-net-art/2-Project-3/js/Main.js
@@ -3,15 +3,53 @@
 const header = document.querySelector("#header");
 const changeHeaderButton = document.querySelector("#change-header-button");
 const changeThemeButton = document.querySelector("#change-theme-button");
+
+//test
 const img1 =document.querySelector ("#img1");
 const img2 =document.querySelector ("#img2");
-const img3 =document.querySelector ("#img3");
 
+//real
+
+const c1 =document.querySelector ("#co1");
+const c11 =document.querySelector ("#co11");
+
+const c2 =document.querySelector ("#co1");
+const c22 =document.querySelector ("#co22");
+
+const c3 =document.querySelector ("#co3");
+const c33 =document.querySelector ("#co33");
+
+const c4 =document.querySelector ("#co4");
+const c44 =document.querySelector ("#co44");
+
+const c5 =document.querySelector ("#co5");
+const c55 =document.querySelector ("#co55");
+
+const c6 =document.querySelector ("#co6");
+const c66 =document.querySelector ("#co66");
+
+const c7 =document.querySelector ("#co7");
+const c77 =document.querySelector ("#co77");
+
+const c8 =document.querySelector ("#co8");
+const c88 =document.querySelector ("#co88");
+
+const c9 =document.querySelector ("#co9");
+const c99 =document.querySelector ("#co99");
+
+const c10 =document.querySelector ("#co10");
+const c100 =document.querySelector ("#co100");
+
+const c0 =document.querySelector ("#co0");
+const c00 =document.querySelector ("#co00");
+
+const c =document.querySelector ("#co");
+const cc =document.querySelector ("#coc");
 
 
 //change header with button click
 changeHeaderButton.addEventListener("click",() => {
-    header.innerHTML = "MEW!!";
+    header.innerHTML = "KEEP CLICKING!";
 })
 
 // Toogle Color theme
@@ -32,14 +70,71 @@ changeThemeButton.addEventListener("click", () => {
     document.body.classList.toggle("dark");
 })
 
-//togle image visivility
+//test
 
-img1.addEventListener("click", () =>{
-    img2.classList.remove("hidden");
-})
-img2.addEventListener("click", () =>{
-    img3.classList.remove("hidden");
-})
-img3.addEventListener("click", () =>{
-    img1.classList.remove("hidden");
-})
+img1.addEventListener("click", () => {
+  img1.classList.add("hidden");     // hide img1
+  img2.classList.remove("hidden");  // show img2
+});
+
+//real work
+
+c1.addEventListener("click", () => {
+  c1.classList.add("hidden");     // hide img1
+  c11.classList.remove("hidden");  // show img2
+});
+
+c2.addEventListener("click", () => {
+  c2.classList.add("hidden");     // hide img1
+  c22.classList.remove("hidden");  // show img2
+});
+
+c3.addEventListener("click", () => {
+  c3.classList.add("hidden");     // hide img1
+  c33.classList.remove("hidden");  // show img2
+});
+
+img1.addEventListener("click", () => {
+  img1.classList.add("hidden");     // hide img1
+  img2.classList.remove("hidden");  // show img2
+});
+
+img1.addEventListener("click", () => {
+  img1.classList.add("hidden");     // hide img1
+  img2.classList.remove("hidden");  // show img2
+});
+
+img1.addEventListener("click", () => {
+  img1.classList.add("hidden");     // hide img1
+  img2.classList.remove("hidden");  // show img2
+});
+
+img1.addEventListener("click", () => {
+  img1.classList.add("hidden");     // hide img1
+  img2.classList.remove("hidden");  // show img2
+});
+
+img1.addEventListener("click", () => {
+  img1.classList.add("hidden");     // hide img1
+  img2.classList.remove("hidden");  // show img2
+});
+
+img1.addEventListener("click", () => {
+  img1.classList.add("hidden");     // hide img1
+  img2.classList.remove("hidden");  // show img2
+});
+
+img1.addEventListener("click", () => {
+  img1.classList.add("hidden");     // hide img1
+  img2.classList.remove("hidden");  // show img2
+});
+
+img1.addEventListener("click", () => {
+  img1.classList.add("hidden");     // hide img1
+  img2.classList.remove("hidden");  // show img2
+});
+
+img1.addEventListener("click", () => {
+  img1.classList.add("hidden");     // hide img1
+  img2.classList.remove("hidden");  // show img2
+});
