15c760ee725f0e753325276cd238b9f22a08ba68
stephaniecortescedillo <stephanie.cortescedillo@sjsu.edu>
2026-05-14 17:33:23 -0700

    F.22

---
+++ tutorials/06-final/Code/other/profile/webcam.js
@@ -0,0 +1,19 @@
+
+
+const webcamVideo = document.querySelector("#webcam");
+
+async function startWebcam(params) {
+    try{
+    const stream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: false
+});
+
+    webcamVideo.srcObject = stream;
+
+
+    }catch(error){
+        console.log("error accessing webcam");
+    }
+}
+startWebcam();
