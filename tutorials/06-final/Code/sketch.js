b689305cef33377baff12b24376e42a4a91bee95
stephaniecortescedillo <stephanie.cortescedillo@sjsu.edu>
2026-05-14 21:31:41 -0700

    F.3

--- tutorials/06-final/Code/sketch.js
+++ tutorials/06-final/Code/sketch.js
@@ -88,3 +88,19 @@ function touchMoved() {
 }
 
 
+
+function mousePressed() {
+
+  for (let p of pd) {
+
+    if (
+      mouseX > p.x &&
+      mouseX < p.x + p.w &&
+      mouseY > p.y &&
+      mouseY < p.y + p.h
+    ) {
+
+      message = p.text;
+    }
+  }
+}
